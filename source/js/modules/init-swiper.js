6eb0fbcad002cd091703fc7a09914ec7e9e787b1
Sergey <serbog230797@gmail.com>
2023-08-31 22:02:58 +0300

    btn-swiper

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -1,8 +1,18 @@
 export const initSwiperIntro = () => {
   const swiperIntro = new window.Swiper('.hero__swiper', {
+    loop: true,
+    speed: 300,
     pagination: {
       el: '.hero__pagination',
       clickable: true,
+    },
+    breakpoints: {
+      320: {
+        allowTouchMove: true,
+      },
+      768: {
+        allowTouchMove: false,
+      },
     },
   });
 
@@ -15,10 +25,12 @@ export const initSwiperUpcomingTours = () => {
       320: {
         slidesPerView: 1,
         spaceBetween: 15,
+        allowTouchMove: true,
       },
       768: {
         slidesPerView: 2,
         spaceBetween: 18,
+        allowTouchMove: false,
       },
       1200: {
         slidesPerView: 3,
