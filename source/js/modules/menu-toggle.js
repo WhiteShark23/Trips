65635fe292e356afc66f8db6e8b1f698e710a085
Sergey <serbog230797@gmail.com>
2023-09-18 16:52:46 +0300

    menu

--- source/js/modules/menu-toggle.js
+++ source/js/modules/menu-toggle.js
@@ -1,8 +1,11 @@
+import '../utils/focus-lock.js';
+
 const nojs = document.querySelector('.nojs');
 const btnHeaderElement = document.querySelector('.js-toggle');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const linkHeaderElements = header.querySelectorAll('.navigation__item');
+let isFocusLocked = false;
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
@@ -17,11 +20,19 @@ const onToggleClick = (evt) => {
 
   header.classList.toggle('show-menu');
   body.classList.toggle('is-active');
+  isFocusLocked = !isFocusLocked;
+
+  if (isFocusLocked) {
+    window.focusLock.unlock('.header');
+  } else {
+    window.focusLock.lock('.header');
+  }
 };
 
 const onCloseMenuClick = () => {
   header.classList.remove('show-menu');
   body.classList.remove('is-active');
+  window.focusLock.unlock('.header');
 };
 
 export const getMenuToggle = () => {
